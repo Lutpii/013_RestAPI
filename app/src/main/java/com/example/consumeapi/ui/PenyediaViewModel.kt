a9237608cbb071581cc1e848405105a056e1e479
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2023-12-28 11:10:01 +0700

    add initializer insert

--- app/src/main/java/com/example/consumeapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/PenyediaViewModel.kt
@@ -5,12 +5,16 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.consumeapi.KontakAplikation
+import com.example.consumeapi.ui.home.screen.InsertViewModel
 import com.example.consumeapi.ui.home.viewmodel.HomeViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepository)
         }
     }
 }
